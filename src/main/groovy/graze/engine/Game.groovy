d67c207475cc0411a4be6c48cc6c4b36e35753fc
Thierry Lacour <thi@praqma.net>
2018-12-10 13:47:13 +0100

    Run 100 games as part of the pipeline

--- src/main/groovy/graze/engine/Game.groovy
+++ src/main/groovy/graze/engine/Game.groovy
@@ -21,26 +21,26 @@ class Game {
     }
 
     void run() {
-        def dayCount = 0
-        def gameOver = false
+        int dayCount = 0
+        boolean gameOver = false
         while (!gameOver) {
             dayCount++
             doTurn(dayCount)
             gameOver = endGame()
 
-            if (config["canvas.enabled"]) {
+            if (config["canvas.enabled"] == "true") {
                 Canvas.instance.title "Dawn of day $dayCount"
                 Canvas.instance.paint()
             }
 
             if (!gameOver) {
-                System.sleep(config["loop.sleep"])
+                System.sleep(((int) config["loop.sleep"]))
                 continue
             }
 
-            if (!config["canvas.enabled"]) {
-                if (config["loop.king"]) {
-                    println pasture.allCowClasses().first() ?: 'stalemate'
+            if (config["canvas.enabled"] != "true") {
+                if (config["loop.king"] == "true") {
+                    println pasture.allCowClasses()[0] ?: 'stalemate'
                 } else {
                     println dayCount
                 }
@@ -50,14 +50,14 @@ class Game {
 
     boolean endGame() {
         def survivingCows = pasture.allCowClasses()
-        if (config["loop.king"]) {
+        if (config["loop.king"] == "true") {
             switch (survivingCows.size()) {
                 case 0:
                     Canvas.instance.message "Wow. They all died. Stalemate?"
                     return true
                 case 1:
                     Canvas.instance.message "Winner winner chicken dinner!"
-                    Canvas.instance.message "<<< ${survivingCows.first()} >>>"
+                    Canvas.instance.message "<<< ${survivingCows[0]} >>>"
                     return true
                 default:
                     return false
