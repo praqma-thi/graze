250db4cc513040585cd00c8b0eff3fa39cedddec
Thierry Lacour <thi@praqma.net>
2018-12-11 13:33:20 +0100

    Fixes #4 - TtogttoghanCow no longer takes redundant moves

--- src/test/groovy/graze/actors/imp/TtogttoghanCowTest.groovy
+++ src/test/groovy/graze/actors/imp/TtogttoghanCowTest.groovy
@@ -34,4 +34,33 @@ class TtogttoghanCowTest {
             assert cow.move(pasture.surroundingsOf(cow)) != Move.STAND
         }
     }
+
+    @Test
+    void no_redundant_moves() {
+        def pasture = getTestPasture()
+        def cow = new TtogttoghanCow()
+        pasture[1][1].actors.add(cow)
+
+        def lastMove = Move.STAND
+        50.times {
+            def currentMove = cow.move(pasture.surroundingsOf(cow))
+            switch(currentMove) {
+                case Move.MOVE_LEFT:
+                    assert lastMove != Move.MOVE_RIGHT
+                    break
+                case Move.MOVE_RIGHT:
+                    assert lastMove != Move.MOVE_LEFT
+                    break
+                case Move.MOVE_DOWN:
+                    assert lastMove != Move.MOVE_UP
+                    break
+                case Move.MOVE_UP:
+                    assert lastMove != Move.MOVE_DOWN
+                    break
+                default:
+                    throw new Exception("Did not expect move: ${currentMove}")
+            }
+            lastMove = currentMove
+        }
+    }
 }
